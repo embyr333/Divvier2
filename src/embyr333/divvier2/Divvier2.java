1495657c9aab5f1edb4117786a5472630a4eaa48
emb33 <emb33@DESKTOP-34GDV5O>
2022-06-19 22:25:55 +0100

    2nd commit

--- src/embyr333/divvier2/Divvier2.java
+++ src/embyr333/divvier2/Divvier2.java
@@ -1,50 +1,63 @@
 /*
-Objective:
+Objective and proposed approach: See comment atop first commit (220619_1442)
 
-On doing CodingBat's MakeBricks problem, I realised the following might work for a definitive (non-random, guarenteed minimal difference for unlimited collection size) of the Divvier program (though original would still have some notional utility in that it could generate alternative divisions, some of which might also be exact minimal diff)...
-
-Where total = sum of values and halfTotal = half that, want abs(h - minDiff) minimal (as before)
-
-First sort the collection by value of elements: z, y, x... in descending order (z >= y, y >= x etc)
-There may be duplicate/replicate values, e.g. z = y, so then make a Map for easier processing
-i.e. value-frequency pairs e.g. z:2, x:1 ...
-
-If z < halfTotal, add as many z's as possible for sum < h
-(stop when you run out of z's or exceed halfTotal-1)
-Decrement the frequency in the Map for each addition, i.e z:2 becomes z:1 and then z:0
-(Also make a reciprocal Map)
-(If z >= halfTotal already, solution is just z vs the rest of the collection)
-
-Then add as many of the next value as possible in the same manner
-(and if adding a single one would exceed halfTotal, add none)
-
-Then repeat for each sucessive value (until halfTotal-1 exceeded)
-
-Convert the two (reciprocal) maps to collections of the original type for return/reporting
-
-First commit, at date_time  220619_1442
+Second commit, at date_time  220619_2225
  */
 
 package embyr333.divvier2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 class Divvier2
-//class MakeBricks
 {
     public static void main(String[] args)
     {
-        System.out.println(makeBricks(3, 1, 8)); // t
-        System.out.println(makeBricks(3, 1, 9)); // f
-        System.out.println(makeBricks(3, 2, 10)); // t    
-        
-        // Some additional tests I added 
-        System.out.println(makeBricks(4, 0, 5)); // expect: false  
-        System.out.println(makeBricks(5, 0, 5)); // expect: true
-        System.out.println(makeBricks(7, 0, 7)); // expect: true
-    }
+        /**
+         * --User will be entering individual numbers in a GUI field (separated by 
+         * e.g. spaces or commas. For testing processing logic here will supply 
+         * these as a List, so change input format for 'bricks' from two ints 
+         * representing the number of size-1 and size-5 bricks to a List containing
+         * the specified number ofcvalue-5 and value-1 elements 
+         */
+//        System.out.println(makeBricks(3, 1, 8)); // true
+//        System.out.println(makeBricks(3, 1, 9)); // false
+//        System.out.println(makeBricks(3, 2, 10)); // true
+//        System.out.println(makeBricks(4, 0, 5)); // false  
+//        System.out.println(makeBricks(5, 0, 5)); // true
+//        System.out.println(makeBricks(7, 0, 7)); // true
+        System.out.println(makeBricks(Arrays.asList(1, 1, 1, 5), 8)); // true
+        System.out.println(makeBricks(Arrays.asList(1, 1, 1, 5), 9)); // false
+        System.out.println(makeBricks(Arrays.asList(1, 1, 1, 5, 5), 10)); // true
+        System.out.println(makeBricks(Arrays.asList(1, 1, 1, 1, 0), 5)); // false
+        System.out.println(makeBricks(Arrays.asList(1, 1, 1, 1, 1, 0), 5)); // true
+        System.out.println(makeBricks(Arrays.asList(1, 1, 1, 1, 1, 1, 1, 0), 7)); // true
+
+    }    
     
-    static boolean makeBricks(int small, int big, int goal)
+    // --Change first two int method parameters to a List to match above inputs
+//    static boolean makeBricks(int small, int big, int goal)
+    static boolean makeBricks(List<Integer> bricks, int goal)
     {
+        bricks.sort(Comparator.naturalOrder()); // --Put bricks in size order
+       
         int bigBrickCapacity = goal / 5;
+        
+        // --Calculate value of "small" amd "big" variables below 
+        int small = 0;
+        int big = 0;
+        for (Integer brick : bricks)
+        {
+            if (brick.equals(1))
+                ++small;
+        
+            else if (brick.equals(5))
+                ++big;            
+        }    
+        System.out.println("small = " + small + "  big = " + big); // (intermediate check)
+
         int bigBricksUsable = big <= bigBrickCapacity ? big : bigBrickCapacity;
         int smallBricksToUse = goal - 5 * bigBricksUsable;
         
@@ -53,16 +66,3 @@ class Divvier2
         return false;
     }
 }
-/* CodingBat problem https://codingbat.com/prob/p183562, MakeBricks (Java)
-
-We want to make a row of bricks that is goal inches long. 
-We have a number of small bricks (1 inch each) and big bricks (5 inches each). 
-Return true if it is possible to make the goal by choosing from the given bricks. 
-This is a little harder than it looks and can be done without any loops. 
-See also: Introduction to MakeBricks (--I haven't looked at (yet))
-makeBricks(3, 1, 8) → true
-makeBricks(3, 1, 9) → false
-makeBricks(3, 2, 10) → true
-
-Yes, passes all tests here, and extra on the site
- */
